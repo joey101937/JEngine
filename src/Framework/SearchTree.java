abda4c05218f29c04970acfd0f00046312c2c581
joey <g.uydude@yahoo.com>
2018-11-05 19:13:27 -0500

    binary tree progress

--- src/Framework/SearchTree.java
+++ src/Framework/SearchTree.java
@@ -6,6 +6,7 @@
 package Framework;
 
 import GameObjects.GameObject2;
+import GameObjects.SampleBird;
 
 /**
  *
@@ -13,21 +14,65 @@ import GameObjects.GameObject2;
  */
 public class SearchTree {
 
-    private static class Node {
-        Node root = null;
-        
-        public void add(GameObject2 addition){
-            if(root == null){
-                root = new Node(addition);
-                return;
-            }
-            if(addition.ID>=root.value){
-                
-            }
+    public Node root = null;
+
+    public static void main(String[] args) {
+        SearchTree tree = new SearchTree();
+        SampleBird sb1 = new SampleBird(new Coordinate(0,0));
+        SampleBird sb2 = new SampleBird(new Coordinate(0,0));
+        SampleBird sb3 = new SampleBird(new Coordinate(0,0));
+        sb1.name="1";
+        sb2.name = "2";
+        sb3.name = "3";
+        tree.add(sb2);
+        tree.add(sb1);
+        tree.add(sb3);
+        tree.printAll();
+    }
+    
+    public void printAll(){
+        if(root == null){
+            System.out.println("null root");
+            return;
+        }else{
+            printHelper(root);
         }
-        
-        
-        
+    }
+    private void printHelper(Node start){
+        if(start.right!=null){
+            printHelper(start.right);
+        }
+        if(start.left!=null){
+            printHelper(start.left);
+        }
+        System.out.println(start.item.name);
+    }
+    
+    
+    public void add(GameObject2 addition) {
+        if (root == null) {
+            root = new Node(addition);
+            return;
+        }
+        addHelper(root, addition);
+    }
+
+    private void addHelper(Node start, GameObject2 addition) {
+        if (addition.ID >= root.value) {
+            if (start.right != null) {
+                addHelper(start.right, addition);
+            } else {
+                start.right = new Node(addition);
+            }
+        } else if (start.left != null) {
+            addHelper(start.left, addition);
+        } else {
+            start.left = new Node(addition);
+        }
+    }
+
+    private static class Node {
+
         public Node left = null, right = null;
         public GameObject2 item;
         public int value;
@@ -38,6 +83,6 @@ public class SearchTree {
             left = null;
             right = null;
         }
-        
+
     }
 }
