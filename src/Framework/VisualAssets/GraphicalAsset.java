11c3932c7a90e9cca3052ab29179949795452335
Joseph <g.uydude@yahoo.com>
2019-02-01 00:10:54 -0500

    GraphicalAsset interface added

---
+++ src/Framework/VisualAssets/GraphicalAsset.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Framework.VisualAssets;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Represents a graphical asset that can be displayed to the screen
+ * @author Joseph
+ */
+public interface GraphicalAsset {
+    public BufferedImage getCurrentImage();
+    public boolean isAnimated();
+}
