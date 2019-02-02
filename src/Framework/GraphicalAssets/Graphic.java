6e7cd073221167203a2ec443708b965bad329de3
Joseph <g.uydude@yahoo.com>
2019-02-02 14:24:54 -0500

    added javadoc comments

--- src/Framework/GraphicalAssets/Graphic.java
+++ src/Framework/GraphicalAssets/Graphic.java
@@ -12,11 +12,38 @@ import java.awt.image.BufferedImage;
  * @author Joseph
  */
 public interface Graphic {
+    /**
+     * gets either image of sprite or current frame of animated sequence
+     * @return  either image of sprite or current frame of animated sequence
+     */
     public BufferedImage getCurrentImage();
+    /**
+     * weather or not the asset is animated. sprite = false, sequence = true.
+     * @return  weather or not the asset is animated.
+     */
     public boolean isAnimated();
+    /**
+     * releases memory from asset and stops animaion thread
+     */
     public void destroy();
+    /**
+     * creates a duplicate asset based on this graphic object
+     * @return Sprite or Sequence based on this
+     */
     public Graphic copy();
+    /**
+     * Scales asset by a certain amount from its current scale
+     * @param d amount to scale by
+     */
     public void scale(double d);
+    /**
+     * sacles asset to a certain percentage size relative to original size
+     * @param d percentage of original size to scale to
+     */
     public void scaleTo(double d);
+    /**
+     * gets current size percentage of image relative to original size
+     * @return current size percentage of image relative to original size
+     */
     public double getScale();
 }
