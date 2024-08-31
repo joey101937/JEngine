058fe125396e128c933a9f3e15dc0c828c137625
Joseph <g.uydude@yahoo.com>
2024-08-31 18:43:07 -0400

    fog sound

--- src/GameDemo/FogDemo/FogCar.java
+++ src/GameDemo/FogDemo/FogCar.java
@@ -3,6 +3,7 @@ package GameDemo.FogDemo;
 
 import Framework.GameObject2;
 import Framework.GraphicalAssets.Sprite;
+import Framework.Main;
 import Framework.SpriteManager;
 
 /**
@@ -10,6 +11,31 @@ import Framework.SpriteManager;
  * @author guydu
  */
 public class FogCar extends GameObject2 {
+    // num ticks before getting to top speed
+    private double accelleration = Main.ticksPerSecond * 2;
+    private double accellerationStage = 0;
+    private double maxSpeed = 3;
+    
+    @Override
+    public void tick(){
+        double accellarationPercent = accellerationStage / accelleration;
+        this.setBaseSpeed(maxSpeed * accellarationPercent);
+        if(this.velocity.x != 0 && accellerationStage < accelleration) {
+            accellerationStage++;
+        } else if (this.velocity.x == 0) {
+            // lose accumulated accelleration when stopped
+            accellerationStage = 0;
+        }
+        // fade out audio after the car leaves the screen
+        if(!this.isOnScreen() && tickNumber > 1 && tickNumber % 60 == 0) {
+            var sound = FogDemo.engineSound;
+            if(sound.getVolume() < .5f) {
+                sound.setLooping(false);
+            } else {
+                sound.setVolume(sound.getVolume() - .01f);
+            }
+        }
+    }
     
     public FogCar(int x, int y) {
         super(x, y);
--- src/GameDemo/FogDemo/FogDemo.java
+++ src/GameDemo/FogDemo/FogDemo.java
@@ -1,18 +1,22 @@
 
 package GameDemo.FogDemo;
 
+import Framework.Audio.SoundEffect;
 import Framework.Game;
 import Framework.GraphicalAssets.Sequence;
 import Framework.Main;
 import Framework.SpriteManager;
 import Framework.Stickers.AnimatedSticker;
 import Framework.Window;
+import java.io.File;
 
 /**
  *
  * @author guydu
  */
 public class FogDemo {
+    public static SoundEffect engineSound = new SoundEffect(new File("Assets/Sounds/engineIdle.au"));
+    
     public static void main(String[] args) {
         Game game = new Game(SpriteManager.dirtBGNight);
         if(Game.runningOnSmallerScreen()) {
@@ -36,6 +40,10 @@ public class FogDemo {
         game.addObject(new FogObject(1000, 500));
         game.addObject(new FogCar(500,500));
         
+        
+        engineSound.setLooping(true);
+        engineSound.setVolume(.9f);
+        engineSound.start();
         
         // Sticker lightSticker = new Sticker(game, SpriteManager.localizedLight, new Coordinate(500, 500), 5000);
 //        new AnimatedSticker(game, fogSequence, new Coordinate(500, 500), 50000);
