74431be2bf8499e8692d4d6db2b4f2efcb34be99
Joseph <g.uydude@yahoo.com>
2024-09-27 18:12:50 -0400

    add launch4j isntaller, remove old run.bat and .sh

--- run.bat
+++
@@ -1,1 +0,0 @@
-java -Dsun.java2d.d3d=false -Xmx1024m -Dsun.java2d.opengl=false -jar JEngine.jar
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "running JEngine"
-java -Dsun.java2d.d3d=false -Xmx1024m -jar 2DTemplate.jar
