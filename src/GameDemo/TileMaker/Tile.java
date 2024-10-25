8005d8ba9e4e58fb8e9ea7808360acfdc94b2c18
Joseph <g.uydude@yahoo.com>
2024-10-25 19:59:48 -0400

    feat: Add sprite management and selection functionality to Tile class

--- src/GameDemo/TileMaker/Tile.java
+++ src/GameDemo/TileMaker/Tile.java
@@ -1,6 +1,8 @@
 package GameDemo.TileMaker;
 
 import Framework.GameObject2;
+import Framework.GraphicalAssets.Sprite;
+import java.awt.Graphics2D;
 
 /**
  *
@@ -8,8 +10,65 @@ import Framework.GameObject2;
  */
 public class Tile extends GameObject2{
     
+    private Sprite mainSprite;
+    private Sprite selectedSprite;
+    private boolean isSelected = false;
+    
+    public void setOpacity(double d) {
+        mainSprite.setOpacity(d);
+        selectedSprite.setOpacity(d);
+    }
+    
+    public void setMainSprite(Sprite s) {
+        mainSprite = s;
+    }
+    
+    public void setSelectedSprite(Sprite s) {
+        selectedSprite = s;
+    }
+    
+    
+    public Sprite getMainSprite() {
+        return mainSprite;
+    }
+    
+    public Sprite getSelectedSprite(){
+        return selectedSprite;
+    }
+
+    public boolean isIsSelected() {
+        return isSelected;
+    }
+
+    public void setIsSelected(boolean isSelected) {
+        this.isSelected = isSelected;
+    }
+    
     public Tile(int x, int y) {
         super(x, y);
+    }
+    
+    /**
+     * uses reflection to create a new instance of this exact class at the given coordinates
+     * @param x copy's x
+     * @param y copy's y
+     * @return new object of my class
+     */
+    public Tile createCopy(int x, int y) {
+      //todo   
+      return null;
+    }
+    
+    public Tile createCopy() {
+        return createCopy((int)location.x, (int)location.y);
+    }
+    
+    
+    @Override
+    public void tick() {
+        super.tick();
+        if(isSelected) this.setGraphic(selectedSprite);
+        else this.setGraphic(mainSprite);
     }
     
 }
