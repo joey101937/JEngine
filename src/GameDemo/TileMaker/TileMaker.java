4352bfbb2b709830beda4c205842c2c1f103ca7a
Joseph <g.uydude@yahoo.com>
2024-10-25 21:07:51 -0400

    tile progress

--- src/GameDemo/TileMaker/TileMaker.java
+++ src/GameDemo/TileMaker/TileMaker.java
@@ -2,6 +2,7 @@ package GameDemo.TileMaker;
 
 import Framework.Coordinate;
 import Framework.Game;
+import Framework.UI_Elements.Examples.Minimap;
 import Framework.Window;
 import GameDemo.TileMaker.Tiles.GreenTile;
 
@@ -18,8 +19,12 @@ public class TileMaker {
     
     public static void main(String[] args) {
         game = new Game(TileAssetManager.tileBackground);
+        game.getCamera().camSpeed = 10;
+        Minimap minimap = new Minimap(game, new Coordinate(0,0));
         int gridWidth = TileAssetManager.tileBackground.getWidth() / TILE_SIZE;
         int gridHeight = TileAssetManager.tileBackground.getHeight() / TILE_SIZE;
+        System.out.println("grid width is" + gridWidth);
+        System.out.println("grid height is " + gridHeight);
         tileGrid = new Tile[gridWidth][gridHeight];
         
         for(int y = 0; y < gridHeight; y++) {
@@ -31,6 +36,7 @@ public class TileMaker {
         }
         game.addIndependentEffect(new TileRenderer());
         Window.initialize(game);
+        Window.addUIElement(minimap);
         game.setInputHandler(new TileInputHandler());
     }
 }
